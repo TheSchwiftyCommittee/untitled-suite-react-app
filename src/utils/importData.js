119120f75e43149c3614024e59a8b3086e7a50d9
whywesmurfing <raymond.doan94@gmail.com>
2021-07-27 21:35:01 +1000

    Updated with production and development API calls

--- src/utils/importData.js
+++ src/utils/importData.js
@@ -1,10 +1,10 @@
-import {USuiteApi} from "../api/USuiteApi";
+import {USuiteApi, apiUrl} from "../api/USuiteApi";
 
 const importData = async (path) => {
   try {
     const response = await USuiteApi.get(path);
     const data = await response.data;
-    console.log("Data has been loaded");
+    console.log(`${apiUrl} Data has been loaded`);
     return data;
   } catch (error) {
     console.log("Failed to load data");
