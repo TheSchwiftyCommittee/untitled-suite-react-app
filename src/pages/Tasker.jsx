c1ec3aa2ca4844b7be80e94ce3caf543f5da0bc2
whywesmurfing <raymond.doan94@gmail.com>
2021-08-03 14:25:26 +1000

    Formatted the Tasker page and Navbar components

--- src/pages/Tasker.jsx
+++ src/pages/Tasker.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { NavLink, withRouter } from 'react-router-dom'
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import { Typography } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   margin: {
     margin: theme.spacing(1),
@@ -19,47 +19,67 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
   },
   textField: {
-    width: '25ch',
+    width: "25ch",
   },
   paper: {
-    width: '80%',
-    maxWidth: '100ch',
-    padding: '1rem',
+    width: "80%",
+    maxWidth: "100ch",
+    padding: "1rem",
   },
   btncontainer: {
-    margin: '1rem 0rem',
-    display: 'flex',
-    flexDirection: 'column',
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: 'row'
+    margin: "1rem 0rem",
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
     },
   },
   btn: {
-    marginTop: '1rem',
+    marginTop: "1rem",
   },
 }));
 
 const Tasker = (props) => {
-  const { user } = props
+  const { user } = props;
 
   const classes = useStyles();
 
   return (
     <Paper className={classes.paper} elevation={5}>
-      <Typography variant="h4" component="h2" gutterBottom>Lorem ipsum dolor sit amet.</Typography>
-    
-      {!user &&
+      <Typography variant="h4" component="h2" gutterBottom>
+        Tasker
+      </Typography>
+
+      {!user && (
         <Grid container className={classes.btncontainer}>
           <Grid item sm={6}>
-            <Button className={classes.btn} variant="contained" color="secondary" size="large" component={NavLink} to="/signup">Sign Up</Button>
+            <Button
+              className={classes.btn}
+              variant="contained"
+              color="secondary"
+              size="large"
+              component={NavLink}
+              to="/signup"
+            >
+              Sign Up
+            </Button>
           </Grid>
           <Grid item sm={6}>
-            <Button className={classes.btn} variant="contained" color="secondary" size="large" component={NavLink} to="/signin">Sign In</Button>
+            <Button
+              className={classes.btn}
+              variant="contained"
+              color="secondary"
+              size="large"
+              component={NavLink}
+              to="/signin"
+            >
+              Sign In
+            </Button>
           </Grid>
         </Grid>
-      }
+      )}
     </Paper>
-  )
-}
+  );
+};
 
-export default withRouter(Tasker)
+export default withRouter(Tasker);
