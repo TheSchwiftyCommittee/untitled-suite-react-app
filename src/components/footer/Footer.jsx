1fa2cf4c933a70ed6f4d4a100073fbb94653cf20
whywesmurfing <raymond.doan94@gmail.com>
2021-08-07 15:57:03 +1000

    Added in Footer page and the Copyright component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import { Copyright } from './Copyright';
+
+import { Box, Container, Grid, makeStyles, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
+
+const footers = [
+  {
+    title: 'Company',
+    description: ['Team', 'History', 'Contact us', 'Locations'],
+  },
+  {
+    title: 'Features',
+    description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+  },
+  {
+    title: 'Resources',
+    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+];
+
+export const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container maxWidth="md" component="footer" className={classes.footer}>
+        <Grid container spacing={4} justifyContent="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
+  )
+}
