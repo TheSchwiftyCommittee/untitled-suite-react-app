10b93642a24f62dba44073c54b854a8ac33d4911
whywesmurfing <raymond.doan94@gmail.com>
2021-08-07 12:31:03 +1000

    Updated SearchBar as a component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import { alpha, makeStyles } from "@material-ui/core/styles";
+import {InputBase} from '@material-ui/core';
+import {SearchIcon} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.grey['400'], 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.grey['400'], 0.25),
+    },
+    marginLeft: 0,
+    marginBottom: '1rem',
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+}));
+
+export const SearchBar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search List Name…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
+    </>
+  )
+}
--- src/pages/Tasker.jsx
+++ src/pages/Tasker.jsx
@@ -3,17 +3,16 @@ import { withRouter } from "react-router-dom";
 
 import importData from '../utils/importData';
 
-import { alpha, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
 import { ListCard } from "../components/card/ListCard";
 import { AddNewCard } from "../components/card/AddNewCard";
+import { SearchBar } from "../components/SearchBar";
 
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
@@ -22,40 +21,6 @@ const useStyles = makeStyles((theme) => ({
     width: "80%",
     maxWidth: "100ch",
     padding: "1rem",
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.grey['400'], 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.grey['400'], 0.25),
-    },
-    marginLeft: 0,
-    marginBottom: '1rem',
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
   },
 }));
 
@@ -83,19 +48,7 @@ const Tasker = () => {
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search List Name…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
+          <SearchBar />
         </Grid>
         <Grid item xs={12}>
           <Typography variant="h5" component="h2" gutterBottom>
