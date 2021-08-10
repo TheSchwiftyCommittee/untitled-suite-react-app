bd0b9fc4f0b14681bccc2bd5a6fafd48d75fc486
whywesmurfing <raymond.doan94@gmail.com>
2021-08-10 21:27:33 +1000

    Updated the requests for when Deleting the lists and added component for updating lists

--- src/components/tasker/ListCard.jsx
+++ src/components/tasker/ListCard.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
-import { Avatar, ButtonBase, Card, CardActions, CardHeader, IconButton } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { orange } from '@material-ui/core/colors';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { orange } from "@material-ui/core/colors";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import deleteData from "../../utils/deleteData";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
   card: {
     width: "100%",
   },
@@ -20,41 +23,77 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: orange[600],
   },
   heading: {
-    textAlign: 'left',
+    textAlign: "left",
+  },
+  cardActions: {
+    justifyContent: "flex-end",
   },
 }));
 
 export const ListCard = (props) => {
-  const { list } = props
+  const { list, setOpenPopup, setErrors } = props;
   const classes = useStyles();
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClickUpdateList = () => {
+    setOpenPopup(true);
+    setAnchorEl(null);
+  }
+
+  const handleClickDeleteList = async (id) => {
+    let formData = new FormData();
+    formData.append("username", localStorage.getItem("username"));
+    formData.append("id", id);
+
+    const config = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      data: formData,
+    };
+    try {
+      const data = await deleteData(`/lists/${id}`, config);
+      console.log(data);
+      setAnchorEl(null);
+      setTimeout(() => {
+        history.push("/tasker");
+      }, 1000);
+    } catch (error) {
+      setErrors(error.message);
+    }
+  };
 
   return (
-    <ButtonBase className={classes.root} component={NavLink} to="#">
-      <Card className={classes.card} elevation={2}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="list" className={classes.avatar}>
-              {list.title[0].toUpperCase()}
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={list.title}
-          titleTypographyProps={{variant: "h6"}}
-          className={classes.heading}
-        />
-        <CardActions disableSpacing>
-          <IconButton aria-label="rename list">
-            <EditIcon />
+    <Card className={classes.card} elevation={2}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="list" className={classes.avatar}>
+            {list.title[0].toUpperCase()}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon onClick={(e) => setAnchorEl(e.currentTarget)} />
           </IconButton>
-          <IconButton aria-label="delete list">
-            <DeleteIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </ButtonBase>
-  )
-}
+        }
+        title={
+          <NavLink to="/" style={{ color: "black", textDecoration: "none" }}>
+            {list.title}
+          </NavLink>
+        }
+        titleTypographyProps={{ variant: "h6" }}
+        className={classes.heading}
+      />
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={() => setAnchorEl(null)}
+      >
+        <MenuItem onClick={() => handleClickUpdateList()}>Update List</MenuItem>
+        <MenuItem onClick={() => handleClickDeleteList(list.id)}>Delete List</MenuItem>
+      </Menu>
+    </Card>
+  );
+};
--- src/pages/Tasker.jsx
+++ src/pages/Tasker.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 
-import getData from '../utils/getData';
+import getData from "../utils/getData";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -10,7 +10,7 @@ import { Typography } from "@material-ui/core";
 import { ListCard } from "../components/tasker/ListCard";
 import { AddNewCard } from "../components/tasker/AddNewCard";
 import { SearchBar } from "../components/SearchBar";
-
+import Popup from "../components/Popup";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,57 +26,74 @@ const useStyles = makeStyles((theme) => ({
 
 const Tasker = () => {
   const classes = useStyles();
-  const [lists, setLists] = useState(null)
-  const [searchInput, setSearchInput] = useState("")
+  const [lists, setLists] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [errors, setErrors] = useState("")
+  const [openPopup, setOpenPopup] = useState(false)
 
   const getLists = async () => {
     const config = {
       headers: {
-        "Authorization": "Bearer " + localStorage.getItem("jwt")
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
       params: {
-        "username": localStorage.getItem("username")
-      }
+        username: localStorage.getItem("username"),
+      },
     };
 
-    const data = await getData("/lists", config)
-    console.log(data)
-    let listsArray = await data
-    setLists(listsArray)
-  }
-  
+    const data = await getData("/lists", config);
+    console.log(data);
+    let listsArray = await data;
+    setLists(listsArray);
+  };
+
   useEffect(() => {
     getLists();
-  }, [])
-
-  // const filteredLists = lists.filter((list) => list.toLowerCase().includes(searchInput.toLowerCase()))
+  }, [lists]);
 
   return (
-    <Paper key="tasker" className={classes.paper} elevation={5}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Tasker
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="h5" component="h2" gutterBottom>
-            Lists
-          </Typography>
-        </Grid>
-        {lists && lists.filter((list) => list.title.toLowerCase().includes(searchInput.toLowerCase())).map((filteredList) => (
-          <Grid item key={filteredList.id} xs={12} md={6}>
-            <ListCard key={filteredList.id} list={filteredList} />
+    <>
+      <Paper key="tasker" className={classes.paper} elevation={5}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Tasker
+            </Typography>
           </Grid>
-        ))}
-        <Grid item xs={12}>
-          <AddNewCard path="/createNewList"/>
+          {errors && <div style={{ color: "red"}} >{errors}</div>}
+          <Grid item xs={12}>
+            <SearchBar
+              searchInput={searchInput}
+              setSearchInput={setSearchInput}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Lists
+            </Typography>
+          </Grid>
+          {lists &&
+            lists
+              .filter((list) =>
+                list.title.toLowerCase().includes(searchInput.toLowerCase())
+              )
+              .map((filteredList) => (
+                <Grid item key={filteredList.id} xs={12} md={6}>
+                  <ListCard key={filteredList.id} list={filteredList} setOpenPopup={setOpenPopup} setErrors={setErrors} />
+                </Grid>
+              ))}
+          <Grid item xs={12}>
+            <AddNewCard path="/createNewList" />
+          </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+      <Popup
+        title="Update List Title"
+        openPopup={openPopup}
+        setOpenPopup={setOpenPopup}
+      >
+      </Popup>
+    </>
   );
 };
 
